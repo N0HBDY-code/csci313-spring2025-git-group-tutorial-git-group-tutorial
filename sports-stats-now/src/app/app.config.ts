251e445da7cde80b8317ba66597c8f515375c856
N0HBDY-code <christopher.m.dick@ndsu.edu>
2025-05-01 22:41:47 -0500

    Added login and registration features

---
+++ sports-stats-now/src/app/app.config.ts
@@ -0,0 +1,25 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCai76hn3cYBiffaICZ86shdXWufuRgN_8",
+  authDomain: "sports-stats-now.firebaseapp.com",
+  projectId: "sports-stats-now",
+  storageBucket: "sports-stats-now.firebasestorage.app",
+  messagingSenderId: "90669414812",
+  appId: "1:90669414812:web:0a1c47a31b30a5ea9ff059",
+  measurementId: "G-G2SRVYZH8R"
+};
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideFirebaseApp(()=>initializeApp(firebaseConfig)),
+    provideAuth(()=>getAuth())
+  ]
+};
