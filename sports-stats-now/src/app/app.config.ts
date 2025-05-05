754757f269f313457f9bfbd7b75bf597dc9e4153
N0HBDY-code <christopher.m.dick@ndsu.edu>
2025-05-05 18:27:17 -0500

    Teams page

--- sports-stats-now/src/app/app.config.ts
+++ sports-stats-now/src/app/app.config.ts
@@ -4,18 +4,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'; 
 
 import { environment } from '../environments/environment';
 
 const firebaseConfig = environment.firebase;
 
-
-
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(()=>initializeApp(firebaseConfig)),
-    provideAuth(()=>getAuth())
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
